a7f497942c028cfc1840a24510b79e61c1206ab0
Vit <vit.zeman.cz@gmail.com>
2023-10-29 20:02:51 +0100

    Tested the communication over the same network on 2 devices Also added outline for cluster deployment, with starting the Server from the Client at the start

--- cluster/Client.py
+++ cluster/Client.py
@@ -1,6 +1,10 @@
+import argparse
+import os
+import time
+
 from mlsocket import MLSocket
 import cv2
-
+import paramiko
 import numpy as np
 
 HOST = "127.0.0.1"
@@ -9,7 +13,36 @@ PORT = 65432
 # Make an ndarray
 data = np.array([1, 2, 3, 4])
 
+
 # Make a keras model
+def parse_args():
+    # TODO: Not tested
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="127.0.0.1")
+    parser.add_argument("--port", type=int, default=65432)
+    parser.add_argument("--user", type=str, default="zemanvit")
+    parser.add_argument("--password", type=str, default="")
+
+    return parser.parse_args()
+
+
+def start_server(host, port, usr, pwd):
+    # TODO: Not tested
+    # Start the server
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, username=usr, password=pwd)
+    ssh.exec_command("conda activate megapose")
+    ssh.exec_command(f"python Project/megapose/Server.py --host {host} --port {PORT}")
+    time.sleep(1)
+    return ssh
+
+
+def stop_server(ssh):
+    # TODO: Not tested
+    ssh.send(chr(3))
+    ssh.exec_command("conda deactivate")
+    ssh.close()
 
 
 def fce(socket, img: np.ndarray, bbox: np.ndarray, id: np.ndarray):
@@ -23,25 +56,60 @@ def fce(socket, img: np.ndarray, bbox: np.ndarray, id: np.ndarray):
 
 # cv2.namedWindow("TestClient", cv2.WINDOW_NORMAL)
 # Send data
-with MLSocket() as s:
-    s.connect((HOST, PORT))  # Connect to the port and host
-    img = cv2.imread("aruco_pattern/board.png")
-    print(img.shape)
-    print(type(img))
-    while True:
-        # s.send(img)  # Send the data
+# with MLSocket() as s:
+#     s.connect((HOST, PORT))  # Connect to the port and host
+#     img = cv2.imread("aruco_pattern/board.png")
+#     print(img.shape)
+#     print(type(img))
+#     while True:
+#         # s.send(img)  # Send the data
 
-        # bbox = np.array([1, 2, 3, 4])
-        # s.send(bbox)
+#         # bbox = np.array([1, 2, 3, 4])
+#         # s.send(bbox)
 
-        # id = np.array((1))
-        # s.send(id)
+#         # id = np.array((1))
+#         # s.send(id)
 
-        # # Receive the data
-        # data = s.recv(1024)q
+#         # # Receive the data
+#         # data = s.recv(1024)q
 
-        # print(f"Data received: {data.shape}")
+#         # print(f"Data received: {data.shape}")
 
-        pose = fce(s, img, np.array([1, 2, 3, 4]), np.array([1]))
+#         pose = fce(s, img, np.array([1, 2, 3, 4]), np.array([1]))
 
-        print(f"Data received: {pose}")
+#         print(f"Data received: {pose}")
+
+if __name__ == "__main__":
+    args = parse_args()
+    host = args.host
+    port = args.port
+    usr = args.user
+    pwd = args.password
+
+    # Start the server at the given host
+    start_server(host, usr, pwd)
+
+    with MLSocket() as s:
+        s.connect((HOST, PORT))  # Connect to the port and host
+        img = cv2.imread("aruco_pattern/board.png")
+        print(img.shape)
+        print(type(img))
+        while True:
+            # s.send(img)  # Send the data
+
+            # bbox = np.array([1, 2, 3, 4])
+            # s.send(bbox)
+
+            # id = np.array((1))
+            # s.send(id)
+
+            # # Receive the data
+            # data = s.recv(1024)q
+
+            # print(f"Data received: {data.shape}")
+
+            pose = fce(s, img, np.array([1, 2, 3, 4]), np.array([1]))
+
+            print(f"Data received: {pose}")
+
+    # Stop the server
--- cluster/Server.py
+++ cluster/Server.py
@@ -3,10 +3,21 @@ import numpy as np
 import time
 import cv2
 
+import argparse
+
+# HOST = "192.168.1.156" # My home IP
 HOST = "127.0.0.1"
 PORT = 65432
 
 # cv2.namedWindow("TestServer", cv2.WINDOW_NORMAL)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="127.0.0.1")
+    parser.add_argument("--port", type=int, default=65432)
+
+    return parser.parse_args()
 
 
 def process_iteration(socket):
@@ -21,30 +32,46 @@ def process_iteration(socket):
 
     # Sent back the data
     # TODO: Add function to process the data and get the pose(quaternion + translation)
+    # TODO: Bassically write in megapose function to get the pose
     pose = np.array([1, 2, 3, 4, 5, 6, 7])  # quaternion + translation
     socket.send(pose)
     return
 
 
-with MLSocket() as s:
-    s.bind((HOST, PORT))
+# with MLSocket() as s:
+#     s.bind((HOST, PORT))
 
-    s.listen()
-    conn, address = s.accept()
+#     s.listen()
+#     conn, address = s.accept()
 
-    with conn:
-        while True:
-            # data = conn.recv(1024)
+#     with conn:
+#         while True:
+#             # data = conn.recv(1024)
 
-            # print(f"Data received: {data.shape}")
-            # cv2.imshow("TestServer", data)
+#             # print(f"Data received: {data.shape}")
+#             # cv2.imshow("TestServer", data)
 
-            # # recieve another bbox
-            # bbox = conn.recv(1024)
-            # print(f"Data received: {bbox}")
+#             # # recieve another bbox
+#             # bbox = conn.recv(1024)
+#             # print(f"Data received: {bbox}")
 
-            # # Sent back the data
-            # data[:, :, :2] = 0
-            # conn.send(data[::-1])
+#             # # Sent back the data
+#             # data[:, :, :2] = 0
+#             # conn.send(data[::-1])
 
-            process_iteration(conn)
+#             process_iteration(conn)
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    host = args.host
+    port = args.port
+
+    with MLSocket() as s:
+        s.connect((HOST, PORT))
+
+        s.listen()
+        conn, address = s.accept()
+        with conn:
+            while True:
+                process_iteration(conn)
