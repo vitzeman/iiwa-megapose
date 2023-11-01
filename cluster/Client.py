31853c901cc27f38b32530403520e1ca1a750fd9
vitzeman <vit.zeman.cz@gmail.com>
2023-11-01 14:52:45 +0100

    Finished testing the server communication

--- cluster/Client.py
+++ cluster/Client.py
@@ -1,10 +1,11 @@
 import argparse
 import os
 import time
+import json
 
 from mlsocket import MLSocket
 import cv2
-import paramiko
+# import paramiko
 import numpy as np
 
 HOST = "127.0.0.1"
@@ -13,6 +14,16 @@ PORT = 65432
 # Make an ndarray
 data = np.array([1, 2, 3, 4])
 
+LABELS = {
+    1: "d01_controller",
+    2: "d02_servo",
+    3: "d03_main",
+    4: "d04_motor",
+    5: "d05_axle_front",
+    6: "d06_battery",
+    7: "d07_axle_rear",
+    8: "d08_chassis",
+}
 
 # Make a keras model
 def parse_args():
@@ -22,28 +33,27 @@ def parse_args():
     parser.add_argument("--port", type=int, default=65432)
     parser.add_argument("--user", type=str, default="zemanvit")
     parser.add_argument("--password", type=str, required=True)
-    parser.add_argument("--init_server", type=bool, default=False)
 
     return parser.parse_args()
 
 
-def start_server(host, port, usr, pwd):
-    # TODO: Not tested
-    # Start the server
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(host, username=usr, password=pwd)
-    ssh.exec_command("conda activate megapose")
-    ssh.exec_command(f"python Project/megapose/Server.py --host {host} --port {port}")
-    time.sleep(1)
-    return ssh
+# def start_server(host, port, usr, pwd):
+#     # TODO: Not tested
+#     # Start the server
+#     ssh = paramiko.SSHClient()
+#     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#     ssh.connect(host, username=usr, password=pwd)
+#     ssh.exec_command("conda activate megapose")
+#     ssh.exec_command(f"python Project/megapose/Server.py --host {host} --port {port}")
+#     time.sleep(1)
+#     return ssh
 
 
-def stop_server(ssh):
-    # TODO: Not tested
-    ssh.send(chr(3))
-    ssh.exec_command("conda deactivate")
-    ssh.close()
+# def stop_server(ssh):
+#     # TODO: Not tested
+#     ssh.send(chr(3))
+#     ssh.exec_command("conda deactivate")
+#     ssh.close()
 
 
 def proccess_input_data(
@@ -102,21 +112,29 @@ if __name__ == "__main__":
     usr = args.user
     pwd = args.password
 
-    init_server = args.init_server
-    if init_server:
-        # Start the server at the given host
-        start_server(host, usr, pwd)
+    img = cv2.imread("/home/testbed/Projects/iiwa-megapose/megapose6d/local_data/examples/Main/images/img_0000.png")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # CHANGED FOR TO FOLLOW PIL
+    json_path = "/home/testbed/Projects/iiwa-megapose/megapose6d/local_data/examples/Main/inputs/img_0000.json"
+    with open(json_path, "r") as f:
+        input_dict = json.load(f)
+
+    bbox = np.array(input_dict[0]["bbox_modal"])
+    label = np.array([3])
+
+    print(f"image shape: {img.shape}, bbox = {bbox}, label: {label}")
 
     with MLSocket() as s:
-        s.connect((HOST, PORT))  # Connect to the port and host
-        img = cv2.imread("aruco_pattern/board.png")
-        print(img.shape)
-        print(type(img))
+        print(f"Connecting to {host}:{port}")
+        s.connect((host, port))  # Connect to the port and host
+        
         while True:
-            pose = proccess_input_data(s, img, np.array([1, 2, 3, 4]), np.array([1]))
+            pose = proccess_input_data(s, img, bbox, label)
 
-            print(f"Data received: {pose}")
+            quat = pose[:4]
+            transl = pose[4:]
 
-    # Stop the server
-    if init_server:
-        stop_server(ssh)
+            print(f"quat: {quat}, transl: {transl} received")
+
+
+            break
+
